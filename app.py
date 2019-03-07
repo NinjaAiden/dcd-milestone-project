2553d3ea602057268a8ea7a90a4b28605751762a
Aiden Harvey <aidenharvey102@gmail.com>
2019-03-07 15:19:39 +0000

    created link to mongo database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'dcd-cookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:Xb0x3869@ds357955.mlab.com:57955/dcd-cookbook'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def main():
