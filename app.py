bc2cd12a1a925dffc92058103adc2d7b00b60266
Aiden Harvey <aidenharvey102@gmail.com>
2019-03-08 15:33:59 +0000

    added routing to view recipes from list

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'dcd-cookbook'
@@ -13,6 +14,11 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template('recipes.html',
         recipes=mongo.db.recipes.find())
+
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+    the_recipe=mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('viewrecipe.html', recipe=the_recipe)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
