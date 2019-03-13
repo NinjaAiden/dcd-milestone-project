43e56dc5efa33f6792486c1ab6678253956f7c92
Aiden Harvey <aidenharvey102@gmail.com>
2019-03-13 11:35:34 +0000

    added base html design for adding recipes

--- app.py
+++ app.py
@@ -20,6 +20,17 @@ def view_recipe(recipe_id):
     the_recipe=mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template('viewrecipe.html', recipe=the_recipe)
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+        cuisine=mongo.db.cuisine.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
